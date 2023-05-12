eda6c84956e01e3ddfc99339594f2e35408554c5
Leoodaviid <david_ax7@hotmail.com>
2023-05-12 13:45:02 -0300

    chore: production

--- src/app/prisma/prisma.ts
+++ src/app/prisma/prisma.ts
@@ -4,4 +4,4 @@ const globalForPrisma = global as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") globalForPrisma.prisma = prisma;
